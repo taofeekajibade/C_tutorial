80149595cac7863695e0de8631c9b23222d67d97
taofeekajibade <cleargoal.ng@gmail.com>
2023-08-03 21:54:23 +0100

    added files to try linking

---
+++ arithmetic.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    int addition;
+    int subt;
+    int multiply;
+
+    addition = add(89, 26);
+    subt = sub(11024, 8076);
+    multiply = mult(154, 211);
+    printf("The result od the adition of (1889 and 426) is : %d\n", addition);
+    printf("The result od the subtraction of (11024 and 8076) is : %d\n", subt);
+    printf("The result od the multiplication of (154 and 221) is : %d\n", multiply);
+    return (0);
+}
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    int sub(int a, int b)
+    {
+        return a - b;
+    }
+
+    int mult(int a, int b)
+    {
+        return a * b;
+    }
