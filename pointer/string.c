23fed11070973a3670f4b5878cf77f75ee8d2dcf
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-08-12 19:19:09 +0100

    moved files

---
+++ pointer/string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+      int i;
+      char *cp;
+
+      cp = "a string";
+      while(*cp != 0)
+      {
+              putchar(*cp);
+              cp++;
+      }
+      putchar('\n');
+
+      for(i = 0; i < 8; i++)
+              putchar("a string"[i]);
+      putchar('\n');
+      exit(EXIT_SUCCESS);
+}
