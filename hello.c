633e3674ece8f958439b528042498ae74565c433
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-08-28 04:27:37 +0100

    reorganised files in directories

--- hello.c
+++
@@ -1,9 +0,0 @@
-//his program will print the output "Hello, World" after compilation
-
-
-#include <stdio.h>
-
-int main(){
-printf("Hello, World!");
-return 0;
-}
--- main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int add(int a, int b);
-int sub(int a, int b);
-int mult(int a, int b);
-
-#endif
