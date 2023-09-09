b5cad0a53f329d6fa157b7c36f8f86d94937f0eb
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-09-09 05:51:28 +0100

    edited main.c file for curly brace errors

--- myshell/main.c
+++ myshell/main.c
@@ -45,45 +45,46 @@ int main(int argc, char *argv[])
 			}
 			continue;
 		}
-		if (str_cmp(usercmd, "exit") == 0)
+		else if (str_cmp(usercmd, "exit") == 0)
 		{
 			free(user_input);
 			user_input = NULL;
 			break;
 		}
-		if (str_cmp(usercmd, "echo") == 0)
+		else if (str_cmp(usercmd, "echo") == 0)
 		{
 			if (arguments != NULL)
 			{
 				print_f(arguments);
 				print_f("\n");
 			}
-			continue;
-			if (pid == -1)
+		}
+		continue;
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			execve(usercmd, cmd_args, NULL);  
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
 			{
-				perror("fork");
-				exit(EXIT_FAILURE);
-			}
-			if (pid == 0)
-			{
-				execve(usercmd, cmd_args, NULL);  
-				perror("execve");
-				exit(EXIT_FAILURE);
+				exit_status = WEXITSTATUS(status);
+				print_f("Child process exited with status: ");
+				print_f(exit_status == 0? "Success" : "Error");
+				print_f("\n");
 			}
 			else
 			{
-				waitpid(pid, &status, 0);
-				if (WIFEXITED(status))
-				{
-					exit_status = WEXITSTATUS(status);
-					print_f("Child process exited with status: ");
-					print_f(exit_status == 0? "Success" : "Error");
-					print_f("\n");
-				}
-				else
-				{
-					print_f("Child process did not exit normally \n");
-				}
+				print_f("Child process did not exit normally \n");
 			}
 		}
 		free(user_input);
