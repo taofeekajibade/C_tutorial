54e35b8bd906cb2fb67b268222699e92da11bc54
Taofeek Ajibade <cleargoal.ng@gmail.com>
2023-09-10 19:16:46 +0100

    modified files

--- teeshell/main.c
+++ teeshell/main.c
@@ -8,7 +8,7 @@ int main(void)
 	size_t bufsize = 0;
 	ssize_t char_read = 0;
 	char *delim = " \n";
-	int i;
+	int i = 0;
 
 	while (1)
 	{
@@ -21,7 +21,15 @@ int main(void)
 			return (-1);
 		}
 		/* handling basic commands */
+		args = malloc(sizeof(char *) * bufsize);
+		if (args == NULL)
+		{
+			perror("malloc");
+			free(usercmd);
+			return (-1);
+		}
 
+		/* Tokenize usercmd */
 		token = strtok(usercmd, delim);
 		while (token)
 		{
@@ -29,25 +37,31 @@ int main(void)
 			token = strtok(NULL, delim);
 			i++;
 		}
-		if (args != NULL && strcmp(args[0], "exit") == 0) /* handles exit */
+
+		if (i > 0)
 		{
-			print_string("Goodbye...\n");
-			break;
-		}
-		else if (args != NULL && strcmp(args[0], "cd") == 0) /* handles 'cd' */
-		{
-			print_string("Usage: cd <directory>\n");
-		}
-		else
-		{
-			if(chdir(*args) != 0)
+			if (strcmp(args[0], "exit") == 0)
 			{
-				perror("cd");
+				print_string("Goodbye...\n");
+				break;
 			}
-			while (args != NULL)
+			else if (strcmp(args[0], "cd") == 0)
+			{
+				if (i < 2)
+				{
+					print_string("Usage: cd <directory>");
+				}
+				else
+				{
+					if (chdir(args[1]) != 0)
+					{
+						perror("cd");
+					}
+				}
+			}
+			else
 			{
 				print_string("command not found\n");
-				token = strtok(NULL, delim);
 			}
 		}
 		free(usercmd);
