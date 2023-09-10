945b4e24e0ba0e207b7ee54bf69b79f44f82dfe4
taofeekajibade <cleargoal.ng@gmail.com>
2023-09-10 12:50:04 +0100

    added a new shell folder

---
+++ sunday_shell/main.c
@@ -0,0 +1,65 @@
+#include "main.h"
+/* gcc -Wall -Wextra -Werror -pedantic *.c -o hsh */
+int main(int ac, char **argv)
+{
+    char *usercmd = NULL;
+    size_t bufsize = 0;
+    ssize_t char_read = 0;
+    char *usercmd_cpy = NULL;
+    char *delim = " \n";
+    char *token = NULL;
+    int token_sum = 0, i;
+
+    (void)ac;
+
+    while (1)
+    {
+        printf("(new_shell)>>> ");
+
+        /* create a variable to count the number of characters entered by the user */
+        char_read = getline(&usercmd, &bufsize, stdin);
+        if (char_read == -1) /* if user enters no character /ctrl+D/ */
+        {
+            printf("Goodbye...\n");
+            return (-1);
+        }
+        /* create a variable to hold user inputs and allocate memory to it */
+        usercmd_cpy = malloc(sizeof(char)* char_read);
+        if (usercmd_cpy == NULL)
+        {
+            /*perror("malloc"); */
+            return (-1);
+        }
+        /* copy user input into the new variable */
+        strcpy(usercmd_cpy, usercmd);
+
+        /* tokenize user input and save it in a variable token */
+        token = strtok(usercmd, delim);
+        while (token) 
+        {
+            token_sum++; 
+            token = strtok(NULL, delim);
+        }
+        token_sum++; /* total number of token */
+
+        /* argv = malloc(sizeof(char *) * token_sum); */
+        token = strtok(usercmd_cpy, delim); /* tokenize a copy of user input */
+        while (token != NULL)
+        {
+            argv[i] = strdup(token);
+            i++;
+            token = strtok(NULL, delim);
+        }
+        argv[i] = NULL; /* set the last sting to null terminator */
+
+
+        printf("%s\n", usercmd);
+
+        free(usercmd);
+        free(usercmd_cpy);
+        free(argv);
+ 
+    }
+
+    return (0);
+}
---
+++ sunday_shell/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+#endif
