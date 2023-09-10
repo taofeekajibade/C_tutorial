23fedeabaa9f1db153356a99e0508d2075d75553
taofeekajibade <cleargoal.ng@gmail.com>
2023-09-10 03:23:23 +0100

    modified main.c code

--- myshell/2-main.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-int main(int ac __attribute__((unused)), char **argv __attribute__((unused)))
-{
-	return (0);
-}
--- myshell/main.c
+++ myshell/main.c
@@ -6,12 +6,13 @@ int main(int argc, char *argv[])
 	size_t bufsize = 0;
 	char *token = NULL;
 	char **args = NULL;
-	char *delim = " ";
+	char *delim = " \n";
 	pid_t pid;
 	int i = 0, j = 0, status, exit_status;
 
 	while (1)
 	{
+		i = 0;
 		print_f("(weshell) $ ");
 		if (getline(&usercmd, &bufsize, stdin) == -1)
 		{
@@ -19,73 +20,84 @@ int main(int argc, char *argv[])
 			free(usercmd);
 			break;
 		}
-		args = (char**)malloc(sizeof(char*));
+		args = malloc(sizeof(char*));
 		token = strtok(usercmd, delim);
 		while (token != NULL)
 		{
 			args[i] = strdup(token);
 			token = strtok(NULL, delim);
 			i++;
+			/*args = realloc(args, (i+1)*sizeof(char*)); */
 		}
+		/* args[i] = NULL; */
 
 		while (args[i] != NULL)
 		{
-			if (str_cmp(args[0], "cd") == 0 && args[1] != NULL)
+			
+			if (str_cmp(args[0], "cd") == 0)
 			{
-				(chdir(args[1]));
-			}
-			else
-			{
-				chdir(getenv("HOME"));
-
-			}
-			if (str_cmp(args[0], "exit") == 0)
-			{
-				free(usercmd);
-				break;
-			}	
-			else if (str_cmp(args[0], "echo") == 0)
-			{
-				if (args[i] != NULL)
+				if(args[1] != NULL)
 				{
-					print_f(args[i]);
-					print_f("\n");
+					chdir(args[1]);
+				}
+				else
+				{
+					chdir(getenv("HOME"));
 				}
 				continue;
 			}
-		}
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			if (execve(args[1], args, NULL) == -1)
+
+			else if (str_cmp(args[0], "exit") == 0)
 			{
-				perror("execve");
+				free(usercmd);
+				break;
+			}
+
+			else if (str_cmp(args[0], "echo") == 0)
+			{
+				for(j=1; args[j] != NULL; j++)
+				{
+				print_f(args[j]);
+				print_f(" ");
+				}
+				print_f("\n");
+				continue;
+			}
+
+			pid = fork();
+			if (pid == -1)
+			{
+				perror("fork");
 				exit(EXIT_FAILURE);
 			}
-		}
-		else
-		{
-			wait(&status);
-			if (WIFEXITED(status))
+
+			else if (pid == 0)
 			{
-				exit_status = WEXITSTATUS(status);
-				print_f("Child process exited with status: ");
-				print_f(exit_status == 0? "Success" : "Error");
-				print_f("\n");
+				if (execve(args[0], args, NULL) == -1)
+				{
+					perror("execve");
+					exit(EXIT_FAILURE);
+				}
 			}
 			else
 			{
-				print_f("Child process did not exit normally \n");
+				wait(&status);
+				if (WIFEXITED(status))
+				{
+					exit_status = WEXITSTATUS(status);
+					print_f("Child process exited with status: ");
+					print_f(exit_status == 0? "Success" : "Error");
+					print_f("\n");
+				}
+				else
+				{
+					print_f("Child process did not exit normally \n");
+				}
 			}
-		}
-		for (j = 0; j <= i; j++)
-		{
-			free(args[j]);
+			for (j = 0; j < i; j++)
+			{
+				free(args[j]);
+			}
 		}
 		free(args);
 		free(usercmd);
