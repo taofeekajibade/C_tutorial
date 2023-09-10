1ffeef1f5b901b79dd3768c87d485fa1544ae453
taofeekajibade <cleargoal.ng@gmail.com>
2023-09-10 18:17:39 +0100

    added new files

---
+++ sunday_shell/execute.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void execute_file(char **argv)
+{
+    char *usercmd = NULL;
+    char *usercmd_path = NULL;
+
+    if (argv)
+    {
+        usercmd = argv[0];
+        usercmd_path = get_path(usercmd);
+        
+        if (execve(usercmd_path, argv, NULL) == -1)
+        {
+            perror("execve");
+        }
+    }
+}
