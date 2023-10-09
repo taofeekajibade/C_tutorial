b134cfbcdb2f97f1cc41870fbf3037379a0c8eb5
taofeekajibade <cleargoal.ng@gmail.com>
2023-10-09 17:23:50 +0100

    edited files for valgrind

---
+++ testshell/free.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _freedptr - A function to free a double pointer.
+ *
+ * @args: Double pointer to an array of strings.
+ *
+ * Return: Void.
+ */
+
+void _freedptr(char **args)
+{
+	int i = 0;
+
+	while (args[i] != NULL)
+	{
+		free(args[i]);
+		i++;
+	}
+	free(args);
+}
